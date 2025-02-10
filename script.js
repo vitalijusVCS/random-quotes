9c1a0f885edff11d8007f5bd04f70ed251a37089
vitalijusVCS <vitalijus.afrikantovas@gmail.com>
2025-02-10 20:12:14 +0200

    Spliting into js modules

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-// Array of quotes
-const quotes = [
-  {
-    quote: 'The only way to do great work is to love what you do',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: 'Success is not the key to happiness. Happiness is the key to success',
-    author: 'Albert Schweitzer',
-  },
-  {
-    quote: 'In the end, we will remember not the words of our enemies, but the silence of our friends',
-    author: 'Martin Luther King Jr',
-  },
-  {
-    quote: 'Life is really simple, but we insist on making it complicated',
-    author: 'Confucius',
-  },
-  {
-    quote: 'The best way to predict your future is to create it',
-    author: 'Abraham Lincoln',
-  },
-  {
-    quote: 'It is never too late to be what you might have been',
-    author: 'George Eliot',
-  },
-  {
-    quote: 'Success usually comes to those who are too busy to be looking for it',
-    author: 'Henry David Thoreau',
-  },
-  {
-    quote: 'Life is 10% what happens to us and 90% how we react to it',
-    author: 'Charles R. Swindoll',
-  },
-  
-];
+import quotes from '/quotes.js';
 
 // Select the elements
 const quoteElement = document.getElementById('quote');
